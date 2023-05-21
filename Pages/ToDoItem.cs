a1eedd2a6394929a6361623dddf04114d0b3853f
macie <macie@MACIEK>
2023-05-21 15:05:48 +0200

    Add project files.

---
+++ Pages/ToDoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Lsb3.Pages
+{
+    public class ToDoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone{get;set;}=false;
+    }
+}
---
+++ ToDoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Lsb3
+{
+    public class ToDoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
